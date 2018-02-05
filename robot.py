f16bd1dc4f47aa307d1d0a908b32ed2296faa8d8
slowrunner <slowrunner@users.noreply.github.com>
2018-02-05 13:46:22 -0500

    fixed control-c handler

--- robot.py
+++ robot.py
@@ -14,16 +14,14 @@ def main():
     print "Starting rwpi.py Main"
     
     robot=rwpi.RWPi()
-    myPyLib.set_cntl_c_handler(robot.cancel)  # Set CNTL-C handler 
+    robot.set_cntl_c_handler()  # Set CNTL-C to cancel all robot processes
     robot.be_scanner()
   except SystemExit:
-    myPDALib.PiExit()
     print "rwpi.py main: time for threads to quit"
     time.sleep(1)
     print "rwpi.py says: Bye Bye"    
   except:
     print "Exception Raised"
-    # r.cancel()
     traceback.print_exc()
     
 
