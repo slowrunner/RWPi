9773b64e4f2e5b164af6a96777c98fad0546ff90
slowrunner <slowrunner@users.noreply.github.com>
2018-02-04 19:35:35 -0500

    preparing for rwpi robot class

---
+++ robot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#
+# robot.py   test rwpi class robot 
+#
+
+import rwpilib.rwpi as rwpi
+
+
+
+def main():
+    robot = rwpi.RWPi()
+    robot.be_scanner()
+
+
+
+if __name__ == "__main__":
+    main()
