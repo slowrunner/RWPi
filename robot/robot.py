8a267437163705f3a44bfca0e9ecd1dd46093c1a
slowrunner <slowrunner@users.noreply.github.com>
2018-08-12 19:51:40 -0400

    moved robot.py to robot/ added sys.path to run anywhere

--- robot/robot.py
+++ robot/robot.py
@@ -3,26 +3,27 @@
 # robot.py   test rwpi class robot 
 #
 
-import rwpilib.rwpi as rwpi
+import sys
+sys.path.append("/home/pi/RWPi/rwpilib")
+
+import rwpi as rwpi
 import traceback
 import time
 
 def main():
   try:
-    print "Starting rwpi.py Main"
-    
+    print "Starting robot.py Main"
+
     robot=rwpi.RWPi()
     robot.set_cntl_c_handler()  # Set CNTL-C to cancel all robot processes
     robot.be_scanner()
   except SystemExit:
-    print "rwpi.py main: time for threads to quit"
+    print "robot.py main: time for threads to quit"
     time.sleep(1)
-    print "rwpi.py says: Bye Bye"    
+    print "robot.py says: Bye Bye"
   except:
     print "Exception Raised"
     traceback.print_exc()
-    
-
 
 
 if __name__ == "__main__":
